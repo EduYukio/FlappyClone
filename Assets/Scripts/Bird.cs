f8e5993043d8fc71bf9f5ae2490a368e4348b4ed
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2018-11-03 23:45:48 -0200

    Add bird jumping mechanic

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bird : MonoBehaviour {
+    Rigidbody2D body;
+    SpriteRenderer birdSprite;
+    Sprite normalSprite;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float jumpSpeed;
+    public Sprite flappingSprite;
+
+
+    void Start () {
+        body = GetComponent<Rigidbody2D>();
+        birdSprite = GetComponent<SpriteRenderer>();
+
+        normalSprite = birdSprite.sprite;
+    }
+
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            jump();
+        }
+
+    }
+
+    IEnumerator flappingAnimation(float waitTime) {
+        birdSprite.sprite = flappingSprite;
+        yield return new WaitForSeconds(waitTime);
+        birdSprite.sprite = normalSprite;
+    }
+
+    void jump() {
+        body.velocity = new Vector2(0f, jumpSpeed);
+        StartCoroutine(flappingAnimation(0.1f));
+    }
 }
