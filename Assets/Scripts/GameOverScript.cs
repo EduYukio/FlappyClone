577ffd028e26463eaff947f9e06e1414bbaf5122
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2018-11-05 00:24:32 -0200

    Add GameOver menu

---
+++ Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScript : MonoBehaviour {
+
+    public void retryButton() {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void menuButton() {
+        SceneManager.LoadScene("MenuScene");
+    }
+}
